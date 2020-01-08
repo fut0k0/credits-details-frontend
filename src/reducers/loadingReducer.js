74a7c4a841147af5650ca7a42176f6ff5460b5fa
fut0k0 <cccabell@icloud.com>
2020-01-07 16:44:33 -0800

    set up loading reducer

---
+++ src/reducers/loadingReducer.js
@@ -0,0 +1,5 @@
+const loadingReducer = (state=false, action) => {
+	return state
+}
+
+export default loadingReducer
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
 import currentUser from "./currentUserReducer";
 import albums from "./albumsReducer";
+import loading from "./loadingReducer";
 
 const rootReducer = combineReducers({
 	currentUser,
 	albums,
-	loading: false
+	loading
 })
 
 export default rootReducer
