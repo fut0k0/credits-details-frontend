2f205854bad8c65fb973004ccb943b5d7cd03d17
fut0k0 <cccabell@icloud.com>
2020-01-08 16:30:58 -0800

    update login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,58 @@
 import React from "react"
 
 class Login extends React.Component {
+
+	state = {
+		username: "",
+		password: ""
+	}
+
+	handleChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	handleSubmit = event => {
+		event.preventDefault()
+		
+		console.log("submit")
+		
+		this.setState({
+			username: "",
+			password: ""
+		})
+	}
+
 	render() {
 		return (
 			<div>
 				Login
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						username:{" "}
+						<input
+							type="text"
+							name="username"
+							placeholder="(enter username)"
+							value={this.state.username}
+							onChange={this.handleChange} />
+					</label><br />
+					<label>
+						password:{" "}
+						<input
+							type="password"
+							name="password"
+							placeholder="(enter password)"
+							value={this.state.password}
+							onChange={this.handleChange} />
+					</label><br />
+					<input type="submit" />
+				</form>
 			</div>
 		)
 	}
+
 }
 
 export default Login
