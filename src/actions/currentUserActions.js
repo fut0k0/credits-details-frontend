7419a20fb0e2de176cc789310ac204481da71db8
fut0k0 <cccabell@icloud.com>
2020-01-08 17:44:29 -0800

    set up login fetch

--- src/actions/currentUserActions.js
+++ src/actions/currentUserActions.js
@@ -1,6 +1,16 @@
 const login = loginData => (
 	dispatch => {
-		console.log(loginData)
+		fetch("http://localhost:3000/api/v1/login", {
+			method: "POST",
+			credentials: "include",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+			body: JSON.stringify(loginData)
+		})
+			.then(resp => resp.json())
+			.then(userData => console.log(userData))
 	}
 )
 
