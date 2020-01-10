c680932d2ce7bb877522b306478cbed9e630c829
fut0k0 <cccabell@icloud.com>
2020-01-09 16:22:56 -0800

    create signup component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
 	return (
 		<div>
 			App
 			<Login />
+			<Signup />
 		</div>
 	)
 }
