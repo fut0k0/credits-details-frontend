c1ef9b486e25a1f10181e4cef078dfa69caefb40
fut0k0 <cccabell@icloud.com>
2020-01-10 16:37:52 -0800

    create logout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Logout from "./components/Logout";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			App
 			<Login />
 			<Signup />
+			<Logout />
 		</div>
 	)
 }
