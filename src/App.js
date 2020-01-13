d77f99850dda4e9b1435f816990cc8576609d73c
fut0k0 <cccabell@icloud.com>
2020-01-13 15:35:07 -0800

    update app component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Logout from "./components/Logout";
 
-function App() {
-	return (
-		<div>
-			App
-			<Login />
-			<Signup />
-			<Logout />
-		</div>
-	)
+class App extends React.Component {
+
+	render() {
+		return (
+			<div>
+				App<br /><br />
+				<Login /><br />
+				<Signup /><br />
+				<Logout /><br />
+			</div>
+		)
+	}
+
 }
 
 export default App
