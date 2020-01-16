00eb9e9df1c8858c376d6cd775ab18b5524fd234
fut0k0 <cccabell@icloud.com>
2020-01-15 18:26:32 -0800

    update drill container

--- src/containers/DrillContainer.js
+++ src/containers/DrillContainer.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
 import Logout from "../components/Logout";
 
 const DrillContainer = props => {
-	return (
-		<div>
-			<Logout history={props.history} /><br />
-			Drill
-		</div>
-	)
+	if (!props.currentUser) {
+		return null
+	} else {
+		return (
+			<div>
+				<Logout history={props.history} /><br />
+				Drill
+			</div>
+		)
+	}
 }
 
-export default DrillContainer
+export default connect(({ currentUser }) => ({ currentUser }))(DrillContainer)
