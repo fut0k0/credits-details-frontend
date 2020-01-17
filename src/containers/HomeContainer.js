bcbc399097c66431a2d92abd6eccddbb23135038
fut0k0 <cccabell@icloud.com>
2020-01-16 17:04:05 -0800

    update home container

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
+import Info from "../components/Info";
 
 const HomeContainer = props => {
-	return (
-		<div>
-			<Login history={props.history} /><br />
-			<Signup history={props.history} />
-		</div>
-	)
+	if (props.currentUser) {
+		return <Info history={props.history} />
+	} else {
+		return (
+			<div>
+				<Login history={props.history} /><br />
+				<Signup history={props.history} />
+			</div>
+		)
+	}
 }
 
-export default HomeContainer
+export default connect(({ currentUser }) => ({ currentUser }))(HomeContainer)
