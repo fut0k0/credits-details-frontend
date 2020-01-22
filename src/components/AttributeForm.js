18cc6ce40614065b44d4fc3c7056c0d17064fda5
fut0k0 <cccabell@icloud.com>
2020-01-21 17:46:05 -0800

    update attribute form component

--- src/components/AttributeForm.js
+++ src/components/AttributeForm.js
@@ -1,5 +1,18 @@
 import React from "react"
 
+const loadSubmitButton = props => {
+	const enabledButton = <input type="submit" />;
+	const disabledButton = <input type="submit" disabled="disabled" />;
+	
+	if (!props.value) {
+		return disabledButton
+	} else if (props.attribute !== "year" ) {
+		return enabledButton
+	} else {
+		return (parseInt(props.value, 10).toString().length === 4 ? enabledButton : disabledButton)
+	}
+}
+
 const AttributeForm = props => (
 	<div>
 		<form onSubmit={event => {props.onSubmit(event, props.attribute)}}>
@@ -8,11 +21,11 @@ const AttributeForm = props => (
 				<input
 					type="text"
 					name={props.attribute}
-					placeholder="(enter name)" 
+					placeholder={props.attribute === "year" ? "(enter 4 digit year)" : "(enter name)"}
 					value={props.value}
 					onChange={props.onChange} />
 			</label>{" "}
-			{props.value ? <input type="submit" /> : <input type="submit" disabled="disabled" />}
+			{loadSubmitButton(props)}
 		</form>
 	</div>
 )
