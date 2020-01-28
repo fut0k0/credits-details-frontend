83e7985328121fddc6f064841b0eb2f72fcdebfe
fut0k0 <cccabell@icloud.com>
2020-01-27 16:02:46 -0800

    create show album component

---
+++ src/components/ShowAlbum.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const ShowAlbum = ({ album }) => (
+	<div>
+		{album.title + " by " + album.artist.name}
+	</div>
+)
+
+export default ShowAlbum
--- src/containers/ShowAlbums.js
+++ src/containers/ShowAlbums.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
+import ShowAlbum from "../components/ShowAlbum";
 
-const ShowAlbums = () => {
+const ShowAlbums = ({ albums }) => {
 
 	return (
 		<div>
 			Show Albums<br /><br />
+			{albums.map(album => <ShowAlbum key={album.id} album={album} />)}
 		</div>
 	)
 
