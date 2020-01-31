00a47d4ece0fbd1af31ed18151557e45245057c3
fut0k0 <cccabell@icloud.com>
2020-01-30 17:50:57 -0800

    update filter & album form item components

--- src/components/AlbumFormItem.js
+++ src/components/AlbumFormItem.js
@@ -1,21 +1,5 @@
-import React from "react"
-
-const listOptions = data => {
-	data.sort((a, b) => {
-		const nameA = a.name.toUpperCase();
-		const nameB = b.name.toUpperCase();
-
-		if (nameA < nameB) {
-			return -1
-		} else if (nameA > nameB) {
-			return 1
-		} else {
-			return 0
-		}
-	})
-
-	return data.map(datum => <option key={datum.id} value={datum.id}>{datum.name}</option>)
-}
+import React from "react";
+import ListOptions from "./ListOptions";
 
 const AlbumFormItem = ({ onChange, item, value, data }) => (
 	<label>
@@ -23,13 +7,11 @@ const AlbumFormItem = ({ onChange, item, value, data }) => (
 		<select
 			name={item === "format" ? item : item + "_id"}
 			value={value}
-			onChange={onChange} >
+			onChange={onChange}>
 			<option key="default" value="" hidden={true}>(select value)</option>
-			{listOptions(data)}
+			{ListOptions(data)}
 		</select>
 	</label>
 )
 
 export default AlbumFormItem
-
-// check if it is okay to remove value in line 29, might not be needed
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,36 +1,18 @@
-import React from "react"
+import React from "react";
+import ListOptions from "./ListOptions";
 
-const listOptions = data => {
-	data.sort((a, b) => {
-		const nameA = a.name.toUpperCase();
-		const nameB = b.name.toUpperCase();
-
-		if (nameA < nameB) {
-			return -1
-		} else if (nameA > nameB) {
-			return 1
-		} else {
-			return 0
-		}
-	})
-
-	return data.map(datum => <option key={datum.id} value={datum.id}>{datum.name}</option>)
-}
-
-const Filter = ({ onChange, item, data }) => {	
-	return (
-		<div>
-			<label>
-				{item[0].toUpperCase() + item.substring(1) + ": "}
-				<select
-					name={item}
-					onChange={onChange}>
-					<option key="default" value="">(clear)</option>
-					{listOptions(data)}
-				</select>
-			</label>
-		</div>
-	)
-}
+const Filter = ({ onChange, item, data }) => (	
+	<div>
+		<label>
+			{item[0].toUpperCase() + item.substring(1) + ": "}
+			<select
+				name={item}
+				onChange={onChange}>
+				<option key="default" value="">(clear)</option>
+				{ListOptions(data)}
+			</select>
+		</label>
+	</div>
+)
 
 export default Filter
