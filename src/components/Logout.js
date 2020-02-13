2b394920e8a5225c978d4bd0eb3bc8028e47b157
fut0k0 <cccabell@icloud.com>
2020-02-12 16:44:35 -0800

    update logout button css

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,6 +2,6 @@ import React from "react";
 import { connect } from "react-redux";
 import { logout } from "../actions/userActions";
 
-const Logout = ({ logout, history }) => <button onClick={() => logout(history)}>Log out</button>
+const Logout = ({ logout, history }) => <button className="btn-lo" onClick={() => logout(history)}>Log out</button>
 
 export default connect(null, { logout })(Logout)
