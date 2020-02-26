8874b79f9de2cab1366145ecf2778dca3ddb6d7a
fut0k0 <cccabell@icloud.com>
2020-02-26 14:44:01 -0800

    clean code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Navigation = () => {
-	return (
-		<div className="Navigation">
-			<NavLink className="NavLink" activeClassName="NavLink-current" to="/drill" exact>Drill</NavLink>
-			<NavLink className="NavLink" activeClassName="NavLink-current" to="/manage" exact>Manage</NavLink>
-			<NavLink className="NavLink" activeClassName="NavLink-current" to="/info" exact>Info</NavLink>
-		</div>
-	)	
-}
+const Navigation = () => (
+	<div className="Navigation">
+		<NavLink
+			className="NavLink"
+			activeClassName="NavLink-current"
+			to="/drill"
+			exact>
+			Drill
+		</NavLink>
+		<NavLink
+			className="NavLink"
+			activeClassName="NavLink-current"
+			to="/manage"
+			exact>
+			Manage
+		</NavLink>
+		<NavLink
+			className="NavLink"
+			activeClassName="NavLink-current"
+			to="/info"
+			exact>
+			Info
+		</NavLink>
+	</div>
+)
 
 export default Navigation
--- src/components/Status.js
+++ src/components/Status.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import Logout from "./Logout";
 
-const Status = ({ history, currentUser }) => (
+const Status = ({ currentUser, history }) => (
 	<div className="Status">
 		Logged in: {currentUser.username + " / "}
 		<Logout history={history} />
